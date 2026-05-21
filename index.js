c12c3582397cd06494e0ce228a340e64831f621b
TRANZCEND3NCE <tranzcend3nce@gmail.com>
2026-05-21 03:13:10 -0400

    second commit

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const myLibrary = [];
 
-function book() {
-
+function Book(title, author, pages, read) {
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.read = read;
+	this.id = crypto.randomUUID();
 }
 
 
